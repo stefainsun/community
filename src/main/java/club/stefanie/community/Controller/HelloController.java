83a3770fb7be707f2baa3a28d742a758426f5711
Wang <641363731@qq.com>
2019-09-26 22:01:38 +0800

    repo

---
+++ src/main/java/club/stefanie/community/Controller/HelloController.java
@@ -0,0 +1,16 @@
+package club.stefanie.community.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+    @RequestMapping("/hello")
+
+    public String hello(@RequestParam(name = "name")String name,Model model){
+        model.addAttribute("name",name);
+        return "hello";
+    }
+}
