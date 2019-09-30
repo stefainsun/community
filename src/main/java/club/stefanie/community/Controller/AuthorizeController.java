85ef5a44767719287ed9738d22ea38ba78271188
wangpanjie <641363731@qq.com>
2019-09-30 22:36:15 +0800

    flyway migration

--- src/main/java/club/stefanie/community/Controller/AuthorizeController.java
+++ src/main/java/club/stefanie/community/Controller/AuthorizeController.java
@@ -11,7 +11,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 @Controller
@@ -29,7 +31,8 @@ public class AuthorizeController {
     @RequestMapping("/callback")
     public String callback(@RequestParam(name = "code")String code,
                            @RequestParam(name = "state")String state,
-                           HttpServletRequest request){
+                           HttpServletRequest request,
+                           HttpServletResponse response){
         AccessTokenDTO accessTokenDTO=new AccessTokenDTO();
         accessTokenDTO.setCode(code);
         accessTokenDTO.setRedirect_uri(redirect_uri);
@@ -41,13 +44,14 @@ public class AuthorizeController {
         String name=githubUser.getName();
         if(name!=null){
             User user = new User();
-            user.setToken(UUID.randomUUID().toString());
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
             user.setName(githubUser.getName());
             user.setAccountId(String.valueOf(githubUser.getId()));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
             userMapper.insert(user);
-            request.getSession().setAttribute("user",githubUser);
+            response.addCookie(new Cookie("token",token));
             return "redirect:/";
         }
         else{
--- src/main/java/club/stefanie/community/Controller/HelloController.java
+++ src/main/java/club/stefanie/community/Controller/HelloController.java
@@ -1,13 +1,39 @@
 package club.stefanie.community.controller;
 
+import club.stefanie.community.mapper.UserMapper;
+import club.stefanie.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class HelloController {
+    @Autowired
+    private UserMapper userMapper;
     @RequestMapping("/")
-    public String hello(Model model){
+    public String hello(Model model,
+                        HttpServletRequest request){
+        Cookie[] cookies = request.getCookies();
+        if(cookies!=null){
+            for (Cookie cookie : cookies) {
+
+                String name = cookie.getName();
+                System.out.println(name);
+                if(name.equals("token")){
+                    String token = cookie.getValue();
+                    User user = userMapper.findByToken(token);
+
+                    if(user!=null){
+                        request.getSession().setAttribute("user",user);
+                    }
+                }
+            }
+        }
+
         return "index";
 
     }
